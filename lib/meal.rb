6b243fe11f079183a26be403921622f60486f241
Saida Davis <saihdavis@gmail.com>
2020-05-05 01:13:54 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,19 @@
+require "pry"
 class Meal
+    attr_accessor :waiter, :customer, :tip, :total
+    @@all = []
+
+    def initialize(waiter, customer, total, tip=0)
+       # binding.pry
+        @waiter = waiter
+        @customer = customer
+        @tip = tip
+        @total = total
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
